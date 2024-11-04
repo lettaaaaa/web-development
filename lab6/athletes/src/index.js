e989a7b7073f4c70233b7396c2a4f03dbd61e65f
Viola <151577839+lettaaaaa@users.noreply.github.com>
2024-11-04 11:10:40 +0200

    upd index.js

--- lab6/athletes/src/index.js
+++ lab6/athletes/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { ProductProvider } from './ProductContext';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <ProductProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ProductProvider>
 );
+
